9d1bb7d88ceaa80c36896ce6bc183f9d5a8c9301
BLDRX <57841656+bldrx@users.noreply.github.com>
2023-05-31 19:25:21 -0400

    Add setting of current year to script

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+document.getElementById('currentYear').innerText = new Date().getFullYear()
 document.getElementById('formSignin').onsubmit = function (event) {
     event.preventDefault()
     alert('Invalid email or password')
